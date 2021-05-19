bd441ef9c7770565dedef1e7ce9320a27400815c
yihanyang <36882989+yihanyang@users.noreply.github.com>
2021-05-19 10:53:42 +0800

    Test
    Components>Button into DashboardPage > App.js > show

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,12 +2,12 @@ import { Component } from 'react';
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-export default class Dashboard extends Component{
+export default class Button1 extends Component{
     render(){
         return(
             <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+            Hello World
+          </Button>
         );
     }
 }
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,2 +1,11 @@
-import Button from '../Components/Button';
+import { Component } from 'react';
+import React from 'react';
+import Button1 from '../Components/Button';
 
+export default class Dashboard extends Component{
+    render(){
+        return(
+            <Button1></Button1>
+        );
+    }
+}
