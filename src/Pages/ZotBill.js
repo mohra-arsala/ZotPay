dd6720438d85e5eaf02a50eb2c1030fc2b0c89c9
Emily  Phee <emilyphee@emilys-air.lan>
2021-05-22 10:29:50 -0700

    First commit on zotbill

--- src/Pages/ZotBill.js
+++ src/Pages/ZotBill.js
@@ -2,12 +2,12 @@ import { Component } from 'react';
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-export default class Dashboard extends Component {
-  render() {
-    return (
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    );
-  }
-}
+const ZotBill = () => {
+  return (
+    <div>
+      <Button>Button1</Button>
+    </div>
+  );
+};
+
+export default ZotBill;
