7697f4e111093fac652d2a966e503f83844d6539
angelinepho <apho8884@gmail.com>
2021-05-21 23:53:46 -0700

    Notification with Delete Swipe (kind of)

--- src/Pages/Notification.js
+++ src/Pages/Notification.js
@@ -8,6 +8,8 @@ import Button from '@material-ui/core/Button';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
+import SwipeableViews from 'react-swipeable-views';
+
 
 export default class Notification extends Component{
     constructor () {
@@ -40,68 +42,91 @@ export default class Notification extends Component{
                 </div>
                 <div className="todayNotif">
                     <h1 className="todayTitle">Today</h1>
-                    <div className="notif">
-                        <div className="notifTop">
-                            <h2 className="notifTitle">Zot Parking</h2>
-                        </div>
-                        <div>
-                            <p className="notifText">Your parking place [6] timing parking session is almost end. Please add more time if you need.</p>
-                            <button className="addTimeBtn" onClick={this.handleOpenModal}>ADD TIME</button>
 
-                            {/* Modal */}
-                            <Modal isOpen={this.state.showModal} className="modal">
-                                <div className="modalTop">
-                                    <CloseIcon onClick={this.handleCloseModal} className="closeIcon"></CloseIcon>
+                    <div className="swipeArea">
+                        <SwipeableViews>
+                            <div className="notif">
+                                <div className="notifTop">
+                                    <h2 className="notifTitle">Zot Parking</h2>
                                 </div>
-                                <div className="modalBottom">
-                                    <div className="centerDiv">
-                                        <div className="timeLeft">
-                                            <p className="timeLeftTitle">Time Left:</p>
-                                            <div className="timeLeftWidget">7 min</div>
-                                        </div>
-                                    </div>
-                                    <div className="durationDiv">
-                                        <p className="setDurationTitle">Set Parking Duration</p>
-                                        <div>
-                                            <div>
-                                                <ArrowDropUpIcon className="upIcon"></ArrowDropUpIcon>
-                                                <ArrowDropUpIcon className="upIcon"></ArrowDropUpIcon>
-                                            </div>
-                                            <div>
-                                                <p className="incrementTimeWidget">00:00</p>
-                                            </div>
-                                            <div>
-                                                <ArrowDropDownIcon className="downIcon"></ArrowDropDownIcon>
-                                                <ArrowDropDownIcon className="downIcon"></ArrowDropDownIcon>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <Button onClick={this.handleCloseModal} variant="contained" style={{backgroundColor: '#0057B2', color: '#FFFFFF'}}>CONFIRM</Button>
-                                </div>
-                            </Modal>
-                            {/* Modal */}
+                                <div>
+                                    <p className="notifText">Your parking place [6] timing parking session is almost end. Please add more time if you need.</p>
+                                    <button className="addTimeBtn" onClick={this.handleOpenModal}>ADD TIME</button>
 
-                        </div>
+                                    {/* Modal */}
+                                    <Modal isOpen={this.state.showModal} className="modal">
+                                        <div className="modalTop">
+                                            <CloseIcon onClick={this.handleCloseModal} className="closeIcon"></CloseIcon>
+                                        </div>
+                                        <div className="modalBottom">
+                                            <div className="centerDiv">
+                                                <div className="timeLeft">
+                                                    <p className="timeLeftTitle">Time Left:</p>
+                                                    <div className="timeLeftWidget">7 min</div>
+                                                </div>
+                                            </div>
+                                            <div className="durationDiv">
+                                                <p className="setDurationTitle">Set Parking Duration</p>
+                                                <div>
+                                                    <div>
+                                                        <ArrowDropUpIcon className="upIcon"></ArrowDropUpIcon>
+                                                        <ArrowDropUpIcon className="upIcon"></ArrowDropUpIcon>
+                                                    </div>
+                                                    <div>
+                                                        <p className="incrementTimeWidget">00:00</p>
+                                                    </div>
+                                                    <div>
+                                                        <ArrowDropDownIcon className="downIcon"></ArrowDropDownIcon>
+                                                        <ArrowDropDownIcon className="downIcon"></ArrowDropDownIcon>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <Button onClick={this.handleCloseModal} variant="contained" style={{backgroundColor: '#0057B2', color: '#FFFFFF'}}>CONFIRM</Button>
+                                        </div>
+                                    </Modal>
+                                    {/* Modal */}
+
+                                </div>
+                            </div>
+                            <div className="deleteNotif">
+                                <p className="deleteText">Delete</p>
+                            </div>
+                        </SwipeableViews>
                     </div>
+
                 </div>
                 <div className="pastNotif">
                     <h1 className="pastTitle">2021/4/10</h1>
-                    <div className="notif">
-                        <div className="notifTop">
-                            <h2 className="notifTitle">Zot Bill</h2>
-                        </div>
-                        <div className="notifBottom">
-                            <p className="notifText">Your zotBill is due September 15, 2021 for Fall 2021.</p>
-                        </div>
+                    <div className="swipeArea">
+                        <SwipeableViews>
+                            <div className="notif">
+                                <div className="notifTop">
+                                    <h2 className="notifTitle">Zot Bill</h2>
+                                </div>
+                                <div className="notifBottom">
+                                    <p className="notifText">Your zotBill is due September 15, 2021 for Fall 2021.</p>
+                                </div>
+                            </div>
+                            <div className="deleteNotif">
+                                <p className="deleteText">Delete</p>
+                            </div>
+                        </SwipeableViews>
                     </div>
 
-                    <div className="notif">
-                        <div className="notifTop">
-                            <h2 className="notifTitle">Zot Bill</h2>
-                        </div>
-                        <div className="notifBottom">
-                            <p className="notifText">Your zotBill is due September 15, 2021 for Fall 2021.</p>
-                        </div>
+                    <div className="swipeArea">
+                        <SwipeableViews>
+                            <div className="notif">
+                                <div className="notifTop">
+                                    <h2 className="notifTitle">Zot Bill</h2>
+                                </div>
+                                <div className="notifBottom">
+                                    <p className="notifText">Your zotBill is due September 15, 2021 for Fall 2021.</p>
+                                </div>
+                            </div>
+                            <div className="deleteNotif">
+                                <p className="deleteText">Delete</p>
+                            </div>
+                        </SwipeableViews>
                     </div>
                 </div>
             </div>
