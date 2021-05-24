3874219e27c07123f63bc3a6d22a712a7a1e991e
Emily  Phee <emilyphee@emilys-air.lan>
2021-05-23 17:49:14 -0700

    Add Confirmation page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Dashboard from '../src/Pages/Dashboard';
 import ZotBill from '../src/Pages/ZotBill';
+import ZotBillConfirmation from '../src/Pages/ZotBillConfirmation';
 
 function App() {
   return (
     <div className="App">
       {/* <Dashboard></Dashboard> */}
-      <ZotBill></ZotBill>
+      {/* <ZotBill></ZotBill> */}
+      <ZotBillConfirmation></ZotBillConfirmation>
     </div>
   );
 }
